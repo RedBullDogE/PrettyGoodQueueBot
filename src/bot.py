96fc5bf26221577fb6566cf843a45324ff079cf5
Drew <gsb06092000@gmail.com>
2020-04-19 16:02:51 +0300

    minor changes: callback_query() code optimization

--- src/bot.py
+++ src/bot.py
@@ -24,7 +24,7 @@ def queue_markup():
     return markup
 
 
-def enter_queue(chat_id, queue_id, user_id):
+def enter_queue(chat_id: int, queue_id: int, user_id: int) -> bool:
     if not dbhelper.queue_id_exists_in_chat(chat_id, queue_id):
         raise dbhelper.QueueNotFoundException
 
@@ -35,7 +35,7 @@ def enter_queue(chat_id, queue_id, user_id):
         return False
 
 
-def left_queue(chat_id, queue_id, user_id):
+def left_queue(chat_id: int, queue_id: int, user_id: int) -> bool:
     if not dbhelper.queue_id_exists_in_chat(chat_id, queue_id):
         raise dbhelper.QueueNotFoundException
 
@@ -46,7 +46,7 @@ def left_queue(chat_id, queue_id, user_id):
         return False
 
 
-def queue_output(chat_id: int, queue: list):
+def queue_output(chat_id: int, queue: list) -> str:
     def user_output(user):
         return f"[{user.first_name} {user.last_name}](tg://user?id={user.id})"
 
@@ -58,8 +58,9 @@ def queue_output(chat_id: int, queue: list):
 
 @bot.message_handler(commands=["start"], func=lambda message: message.chat.type == "private")
 def command_start_private_chat(message):
-    bot.send_message(message.chat.id, "Hi! I can only work in groups. Please, add me to the group chat, that needs"
-                                      " a hero of queues!")
+    response_text = "Hi! I can only work in groups. Please, add me to the group chat, that needs"
+    " a hero of queues!"
+    bot.send_message(message.chat.id, response_text)
 
 
 @bot.message_handler(commands=["start"], func=lambda message: message.chat.type == "group")
@@ -101,7 +102,7 @@ def command_create(message):
 @bot.message_handler(commands=["delete"], func=lambda message: message.chat.type == "group")
 def command_delete(message):
     command_split = message.text.split(maxsplit=1)
-    
+
     if len(command_split) != 2:
         response_text = f"You should use the 'delete' command correctly:\n\t/delete QUEUE_NAME"
         bot.send_message(message.chat.id, response_text)
@@ -124,62 +125,63 @@ def command_delete(message):
 
 @bot.callback_query_handler(func=lambda call: call.message.chat.type == "group")
 def callback_query(call):
+    call_id = call.id
+    chat_id = call.message.chat.id
+    queue_id = call.message.message_id
+    user_id = call.from_user.id
+
     if call.data == "cb_enter":
         try:
-            is_queue_entered = enter_queue(
-                call.message.chat.id, call.message.message_id, call.from_user.id)
+            is_queue_entered = enter_queue(chat_id, queue_id, user_id)
         except dbhelper.QueueNotFoundException:
             bot.answer_callback_query(
-                call.id, "Queue does not exist! Please contact your chat administrator!")
+                call_id, "Queue does not exist! Please contact your chat administrator!")
             return
 
         if is_queue_entered:
-            name, queue = dbhelper.get_queue(
-                call.message.chat.id,
-                call.message.message_id
-            )
+            name, queue = dbhelper.get_queue(chat_id, queue_id)
 
-            edited_text = f"Queue: {name}\nNumber of members: {len(queue)}\nMembers:\n{queue_output(call.message.chat.id, queue)}"
+            edited_text = f"Queue: {name}\nNumber of members: {len(queue)}\nMembers:\n{queue_output(chat_id, queue)}"
             bot.edit_message_text(
                 edited_text,
-                call.message.chat.id,
-                call.message.message_id,
+                chat_id,
+                queue_id,
                 reply_markup=queue_markup(),
                 parse_mode="markdown"
             )
             bot.answer_callback_query(call.id)
         else:
             bot.answer_callback_query(
-                call.id, "You are already in this queue!")
+                call_id,
+                "You are already in this queue!"
+            )
     elif call.data == "cb_leave":
         try:
-            is_queue_left = left_queue(
-                call.message.chat.id, call.message.message_id, call.from_user.id)
+            is_queue_left = left_queue(chat_id, queue_id, user_id)
         except dbhelper.QueueNotFoundException:
             bot.answer_callback_query(
-                call.id, "Queue does not exist! Please contact your chat administrator!")
+                call_id,
+                "Queue does not exist! Please contact your chat administrator!"
+            )
             return
 
         if is_queue_left:
-            name, queue = dbhelper.get_queue(
-                call.message.chat.id,
-                call.message.message_id
-            )
+            name, queue = dbhelper.get_queue(chat_id, queue_id)
 
-            edited_text = f"Queue: {name}\nNumber of members: {len(queue)}\nMembers:\n{queue_output(call.message.chat.id, queue)}"
+            edited_text = f"Queue: {name}\nNumber of members: {len(queue)}\nMembers:\n{queue_output(chat_id, queue)}"
             bot.edit_message_text(
                 edited_text,
-                call.message.chat.id,
-                call.message.message_id,
+                chat_id,
+                queue_id,
                 reply_markup=queue_markup(),
                 parse_mode="markdown"
             )
             bot.answer_callback_query(call.id)
         else:
             bot.answer_callback_query(
-                call.id, "You are not in this queue yet!")
+                call_id, "You are not in this queue yet!")
     else:
-        bot.answer_callback_query(call.id, "Something went wrong...")
+        bot.answer_callback_query(call_id, "Something went wrong...")
 
 
 if __name__ == "__main__":
